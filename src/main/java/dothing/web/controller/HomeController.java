98487d19232d6325ea7c1f42f17781b20f999eb8
MSDN-SPECIAL\Administrator <hklk@naver.com>
2017-06-02 18:14:15 +0900

    기본 첫 셋팅

---
+++ src/main/java/dothing/web/controller/HomeController.java
@@ -0,0 +1,21 @@
+package dothing.web.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("A");
+		return "home";
+	}
+
+}
