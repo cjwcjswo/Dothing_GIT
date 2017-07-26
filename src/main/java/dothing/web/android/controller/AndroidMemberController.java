c70800c1196d24ff361ea2e2538a6305c7cfe371
ekfkfl <shbogu@naver.com>
2017-07-27 07:43:45 +0900

    성훈 페이스북 연동 끝

--- src/main/java/dothing/web/android/controller/AndroidMemberController.java
+++ src/main/java/dothing/web/android/controller/AndroidMemberController.java
@@ -1,10 +1,13 @@
 package dothing.web.android.controller;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -32,9 +35,10 @@ public class AndroidMemberController {
 	@RequestMapping("/checkId")
 	@ResponseBody
 	public MemberDTO android(HttpServletRequest request) {
-		String email = request.getParameter("email");
+		String email = request.getParameter("userId");
 		String password = request.getParameter("password");
 		String token = request.getParameter("token");
+		
 		if (email == null || password == null) {
 			System.out.println("입력값 부족");
 			return null;
@@ -56,6 +60,54 @@ public class AndroidMemberController {
 		}
 		
 		return memberDTO;
+	}
+	
+	@RequestMapping("/apiCheckId")
+	@ResponseBody
+	public MemberDTO apiCheckId(HttpServletRequest request,MemberDTO memberDTO) throws Exception{
+		String email = memberDTO.getUserId();
+		String password = memberDTO.getPassword();
+		String token = request.getParameter("token");
+		String facebook = request.getParameter("facebook");
+		
+		if (email == null || password == null) {
+			System.out.println("입력값 부족");
+			return null;
+		}
+		MemberDTO resultMemberDTO = memberService.selectMemberById(email);
+
+		if (resultMemberDTO == null && facebook == null) {
+			System.out.println("그런얘읎다");
+			return null;
+		}
+		if(facebook != null){
+			System.out.println("*********"+password);
+			System.out.println("facebook 들어옴");
+			URL url = new URL(memberDTO.getSelfImg());
+			BufferedImage img = ImageIO.read(url);
+			memberDTO.setSelfImg("profile.jpg");
+			HttpSession session = request.getSession();
+			String path = session.getServletContext().getRealPath("") + "\\users\\" + memberDTO.getUserId();
+			File folder = new File(path);
+			folder.mkdirs();
+			File file = new File(path + "\\" + memberDTO.getSelfImg());
+			ImageIO.write(img, "jpg", file);
+			
+			androidService.androidSignIn(memberDTO);	
+			resultMemberDTO = memberService.selectMemberById(email);
+		}
+
+		if (!passwordEncoder.matches(password, resultMemberDTO.getPassword())) {
+			System.out.println("비번노일치");
+			return null;
+		}
+		if (token != null) {
+			androidService.insertToken(resultMemberDTO.getUserId(), token);
+			System.out.println("토큰 추가: " + token);
+		}
+		
+		
+		return resultMemberDTO;
 	}
 
 	@RequestMapping("/isSafety")
